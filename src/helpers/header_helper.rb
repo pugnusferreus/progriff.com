4249247005657b962f35feeade2117306e476bfc
Benson Lim <benson@spawncamp.net>
2010-01-05 16:01:27 +0800

    trying jmp3

--- src/helpers/header_helper.rb
+++ src/helpers/header_helper.rb
@@ -8,4 +8,41 @@ module HeaderHelper
     }
   end
   
+  def index_js()
+    return <<-HTML
+      <script type='text/javascript'>
+        $(document).ready(function() {
+            $('#if_i_were_human_div').hide();
+            $('#run_away_div').hide();
+            $('#this_possible_love_div').hide();
+            
+
+            $('a#if_i_were_human').click(function() {
+              $('#if_i_were_human_div').toggle(400);
+              return false;
+            });  
+
+
+            $('a#run_away').click(function() {
+              $('#run_away_div').toggle(400);
+              return false;
+            });
+
+
+            $('a#this_possible_love').click(function() {
+              $('#this_possible_love_div').toggle(400);
+              return false;
+            });
+            $("#if_i_were_human_player").jmp3({
+              filepath: "music/if%20i%20were%20human.mp3",
+              width: 200,
+              showdownload: "true"
+
+            });
+
+          });
+      </script>
+    HTML
+  end
+
 end
