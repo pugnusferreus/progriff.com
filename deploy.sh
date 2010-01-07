77701870c32f73635221eaefb051643ed5ea2353
Benson Lim <pugnusferreus@spawncamp.net>
2010-01-07 17:28:51 +1000

    changed cp to rsync

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #/bin/sh
 
 staticmatic build .
-cp site/*.html ~/public_html/
-cp site/images/* ~/public_html/images/
-cp site/javascripts/* ~/public_html/javascripts/
-cp site/stylesheets/* ~/public_html/stylesheets/
+rsync -rc site/ ~/public_html/
