c51f479aff667673b64c92460a6fe6be05dff735
Benson Lim <pugnusferreus@Benson-Lims-MacBook-Pro-15.local>
2010-03-23 00:00:12 +0800

    changed the tweet function abit

--- site/javascripts/application.js
+++ site/javascripts/application.js
@@ -64,10 +64,10 @@ $(function()
 		avatar_size: 16,
 		count: 5,
 		auto_join_text_default: "", 
-		auto_join_text_ed: "we",
-		auto_join_text_ing: "we were",
+		auto_join_text_ed: "",
+		auto_join_text_ing: "",
 		auto_join_text_reply: "",
-		auto_join_text_url: "we were checking out",
-		loading_text: "loading tweets..."
+		auto_join_text_url: "",
+		loading_text: "loading twitter..."
 	});
 });
